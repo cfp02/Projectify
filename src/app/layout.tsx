1aa78451626aa2f42a7dd664e6a2ee4ad0551e57
Cole Parks <coleparks13@gmail.com>
2025-02-10 16:22:00 -0500

    Added iconx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,16 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Projectify',
   description: 'Project Management and Portfolio Platform',
+  icons: {
+    icon: [
+      {
+        url: '/icon.svg',
+        type: 'image/svg+xml',
+      }
+    ],
+    shortcut: '/icon.svg',
+    apple: '/icon.svg',
+  },
 };
 
 export default function RootLayout({
